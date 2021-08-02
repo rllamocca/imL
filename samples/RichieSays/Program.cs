37f2b42e06e837e47d175116027eed7d68bf5b2a
Ricardo Llamocca <r.llamocca@outlook.com>
2021-08-01 23:25:02 -0400

    ...

---
+++ samples/RichieSays/Program.cs
@@ -0,0 +1,17 @@
+﻿using imL.Utility;
+using imL.Utility.Terminal;
+using imL.Utility.Terminal.Process;
+
+ConsoleHelper.Starts();
+//################################################################
+
+if (args != null && args.Length > 0)
+{
+    bool _reorganize = args.ArgAppear("REORGANIZE");
+
+    if (_reorganize)
+        Reorganize.Main(args);
+}
+
+//################################################################
+ConsoleHelper.Ends();
