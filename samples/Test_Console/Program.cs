b90ae9963524ee29e9ef9892a2073d59d35caabb
Ricardo Llamocca <r.llamocca@outlook.com>
2021-08-19 22:53:11 -0400

    ...

--- samples/Test_Console/Program.cs
+++ samples/Test_Console/Program.cs
@@ -1,10 +1,6 @@
-﻿using imL.Utility;
-using imL.Utility.Ftp;
-using imL.Utility.Terminal;
+﻿using System;
 
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using imL.Utility.Terminal;
 
 namespace Test_Console
 {
@@ -37,7 +33,7 @@ CTRL_CLOSE_EVENT
             Console.WriteLine("ProcessExit ...");
         }
 
-        async static Task Main(
+        static void Main(
             //string[] _args
             )
         {
@@ -48,23 +44,11 @@ CTRL_CLOSE_EVENT
 
             //################################################################
 
-            using (ElapsedTime _time = new())
-            {
-                FormatFtp _tmp3 = new();
-                _tmp3.Timeout = Convert.ToInt32(TimeSpan.FromSeconds(40).TotalMilliseconds);
-                _tmp3.Host = "ftp://ftp1.microexpertos.cl";
-                _tmp3.Path = "/CV_BCI_RDA_20210729.csv";
-                _tmp3.UserName = "cruzverde";
-                _tmp3.Password = "CruzVerde2016.";
 
-                Stream _ms = new MemoryStream();
-                FtpHelper.From_DownloadFile(ref _ms, _tmp3);
-                _ms.FileCreate("C:\\tmp\\CV_BCI_RDA_20210729.csv");
-            }
             //################################################################
 
 #if DEBUG
-            ConsoleHelper.Ends(true, false);
+            ConsoleHelper.PressAnyKeyToExit();
 #else
             ConsoleHelper.Ends();
 #endif
