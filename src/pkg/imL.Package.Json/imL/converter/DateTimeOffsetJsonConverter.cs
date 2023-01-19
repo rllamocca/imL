6d98cffdc6abd1f84068eedbe618d9f4c87c97d3
Ricardo Llamocca <rllamocca@microexpertos.com>
2023-01-19 17:35:41 -0300

    ...

---
+++ src/pkg/imL.Package.Json/imL/converter/DateTimeOffsetJsonConverter.cs
@@ -0,0 +1,28 @@
+﻿#if (NETSTANDARD2_0_OR_GREATER || NET5_0_OR_GREATER)
+
+using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace imL.Package.Json
+{
+    public class DateTimeOffsetJsonConverter : JsonConverter<DateTimeOffset>
+    {
+        public override DateTimeOffset Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options) =>
+                DateTimeOffset.ParseExact(reader.GetString(),
+                    "MM/dd/yyyy", CultureInfo.InvariantCulture);
+
+        public override void Write(
+            Utf8JsonWriter writer,
+            DateTimeOffset dateTimeValue,
+            JsonSerializerOptions options) =>
+                writer.WriteStringValue(dateTimeValue.ToString(
+                    "MM/dd/yyyy", CultureInfo.InvariantCulture));
+    }
+}
+
+#endif
