b662d6c33121250f00fc5eaadc87b791b6f4e81f
Ricardo Llamocca <rllamocca@microexpertos.com>
2023-03-23 11:42:58 -0300

    ...

--- sdk/console/SAMPLE.imL.Resource/Program.cs
+++ sdk/console/SAMPLE.imL.Resource/Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using imL.Contract;
+using imL;
 using imL.Resource;
 
 IProcessInfo _process = new ProcessInfoDefault(new AppInfoDefault(args));
-_process.AddInserted(1);
+_process.AddInserted();
 _process.Danger(new ArgumentNullException("Richie"));
 string _body = HtmlPattern.Resume(_process);
 
