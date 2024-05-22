093c3e65461a6f2658edc2867b4ed1c370ef317f
Ricardo Llamocca <r.llamocca@outlook.com>
2024-05-22 13:28:16 -0400

    ...

---
+++ src/pkg/imL.Package.MicrosoftSql/imL/helper/init.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using imL.DB;
+
+namespace imL.Package.MicrosoftSql
+{
+    public partial class MicrosoftSqlHelper : IHelper
+    {
+        public IConnection Connection { get; }
+        public bool? Throw { get; }
+        public IProgress<int> Progress { get; }
+
+        public MicrosoftSqlHelper(IConnection _conn, bool _throw = false, IProgress<int> _progress = null)
+        {
+            Connection = _conn;
+            Throw = _throw;
+            Progress = _progress;
+        }
+    }
+}
