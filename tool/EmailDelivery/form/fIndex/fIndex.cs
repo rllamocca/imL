c104fcd009883fa7f7be2d7572e06f4e463f98b3
Ricardo Llamocca <r.llamocca@outlook.com>
2024-06-12 16:27:01 -0400

    ...

---
+++ tool/EmailDelivery/form/fIndex/fIndex.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace EmailDelivery
+{
+    public partial class fIndex : Form
+    {
+        public fIndex()
+        {
+            InitializeComponent();
+        }
+    }
+}
